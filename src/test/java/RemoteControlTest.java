4134ec0990646e4941e379e73d3c0aa6eed4a197
kreisfahrer <kreisfahrer@gmail.com>
2015-05-15 21:32:29 +0300

    Added new tests

---
+++ src/test/java/RemoteControlTest.java
@@ -0,0 +1,32 @@
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RemoteControlTest {
+    private Selenium selenium;
+
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://comaqa.by/");
+        selenium.start();
+    }
+
+    @Test
+    public void testRemote() throws Exception {
+        selenium.open("/en/");
+        selenium.click("link=Events");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("link=Our events");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("link=Education");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("link=About us");
+        selenium.waitForPageToLoad("30000");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+}
---
+++ src/test/java/WebdriverTest.java
@@ -0,0 +1,76 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebdriverTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        baseUrl = "http://comaqa.by/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testJava() throws Exception {
+        driver.get(baseUrl + "/en/");
+        driver.findElement(By.linkText("Events")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Our events")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Education")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("About us")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
